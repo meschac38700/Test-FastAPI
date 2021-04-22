b93b857133436cece6e773891e641e8897d84aba
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-04-22 15:57:47 +0200

    Revert "GitHub actions"

--- app/api/api_v1/models/tortoise.py
+++ app/api/api_v1/models/tortoise.py
@@ -17,9 +17,7 @@ class Person(models.Model):
     country_of_birth = fields.CharField(max_length=50)
 
     def __str__(self):
-        return "{}(name={} {})".format(
-            self.__class__.__name__, self.first_name, self.last_name
-        )
+        return f"{self.__class__.__name__}(name={self.first_name} {self.last_name})"
 
     def __repr__(self):
         return self.__str__()
