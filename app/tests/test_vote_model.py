2420a0ee71fb044aac4eac93c3f859ebcf7545c8
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-08-28 14:49:56 +0200

    Review test_get_votes

--- app/tests/test_vote_model.py
+++ app/tests/test_vote_model.py
@@ -1,9 +1,13 @@
+from itertools import zip_longest
+import concurrent.futures as futures
+
 from fastapi import status
 from httpx import AsyncClient
 from tortoise.contrib import test
 
 from main import app
 from app.api.api_v1 import settings
+from app.api.utils import API_functools
 from app.api.api_v1.storage.initial_data import INIT_DATA
 from app.api.api_v1.models.pydantic import default_content
 from app.api.api_v1.models.tortoise import Person, Comment, Vote
@@ -14,6 +18,42 @@ API_ROOT = "/api/v1/votes/"
 
 
 class TestPersonAPi(test.TestCase):
+    async def insert_votes(
+        self,
+        votes: list[dict],
+        comments: list[dict] = [],
+        users: list[dict] = [],
+    ) -> None:
+        """Test util method: insert some votes data
+
+        Args:
+            comments (list[dict]): list of comments, Default [].
+            users (list[dict]): list of persons, Default [].
+            votes (list[dict]): list of votes , Default [].
+
+        Returns:
+            None
+        """
+        # Insert data
+        with futures.ProcessPoolExecutor() as executor:
+
+            for comment, user, vote in zip_longest(comments, users, votes):
+                if user:
+                    executor.map(
+                        await API_functools._insert_default_data(
+                            "person", user
+                        )
+                    )
+                if comment:
+                    executor.map(
+                        await API_functools._insert_default_data(
+                            "comment", comment
+                        )
+                    )
+                executor.map(
+                    await API_functools._insert_default_data("vote", vote)
+                )
+
     async def test__str__repr__(self):
         user = await Person.create(**INIT_DATA.get("person", [])[0])
         comment = await Comment.create(user=user, content=default_content)
@@ -39,3 +79,30 @@ class TestPersonAPi(test.TestCase):
 
         assert response.status_code == status.HTTP_404_NOT_FOUND
         assert response.json() == expected
+
+        # Insert new Comment
+        votes_inserted = [*INIT_DATA.get("vote", [])[:4]]
+        await self.insert_votes(
+            comments=[{**c} for c in INIT_DATA.get("comment", [])[:4]],
+            users=[{**p} for p in INIT_DATA.get("person", [])[:4]],
+            votes=votes_inserted,
+        )
+
+        async with AsyncClient(app=app, base_url=BASE_URL) as ac:
+            response = await ac.get(API_ROOT)
+        actual = response.json()
+        expected = {
+            "next": None,
+            "previous": None,
+            "votes": [
+                {
+                    "id": pk,
+                    "comment_id": vote["comment"],
+                    "user_id": vote["user"],
+                }
+                for pk, vote in enumerate(votes_inserted, start=1)
+            ],
+        }
+
+        assert response.status_code == status.HTTP_200_OK
+        assert expected == actual
