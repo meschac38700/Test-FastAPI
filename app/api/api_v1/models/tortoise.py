665be2b5bbb7d1189b72b75a17ff1b13a638c9e2
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-09-07 21:13:44 +0200

    Add owner fullname when getting children

--- app/api/api_v1/models/tortoise.py
+++ app/api/api_v1/models/tortoise.py
@@ -61,13 +61,15 @@ class Comment(models.Model):
 
         fields = fields if len(fields) > 0 else API_functools.get_attributes(Comment)
         filter_key = {"top_parent_id" if deep else "parent_id": self.id}
-        return await (
-            Comment.filter(**filter_key)
-            .prefetch_related("vote")
-            .annotate(votes=Count("vote", distinct=True))
-            .annotate(nb_children=Count("children", distinct=True))
-            .order_by(order_by)
-            .values(*fields, "votes", "nb_children")
+        return await API_functools.add_owner_fullname(
+            await (
+                Comment.filter(**filter_key)
+                .prefetch_related("vote")
+                .annotate(votes=Count("vote", distinct=True))
+                .annotate(nb_children=Count("children", distinct=True))
+                .order_by(order_by)
+                .values(*fields, "votes", "nb_children")
+            )
         )
 
     def __str__(self):
